fa508847e1be30ddf9a9c974a65ec74604f370d5
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-03-07 17:28:34 -0500

    First pass at seed data: create base styles, walls, and sample users

--- db/migrate/20200307183932_create_users.rb
+++ db/migrate/20200307183932_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :username
       t.string :password_digest
-      t.integer :total_points
+      t.integer :total_points, deafult: 0
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,65 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Running DB seed..."
+
+# Don't let seed duplicate data more than once
+puts "Dropping DB..."
+Style.destroy_all
+ProblemStyle.destroy_all
+Problem.destroy_all
+Send.destroy_all
+User.destroy_all
+
+
+walls = [
+  "Pipe",
+  "Cave",
+  "Traverse",
+  "Do Not Top Out",
+  "Positivity",
+  "Logo",
+  "Pineapple",
+  "Setter's Closet",
+]
+
+puts "Creating set Walls..."
+walls.each do |wall|
+  Wall.create(name: wall)
+end
+
+styles = [
+  "dyno",
+  "crimps",
+  "jugs",
+  "slopers",
+  "pinches",
+  "pockets",
+  "body tension",
+  "compression",
+  "powerful",
+  "overhang",
+  "flexible",
+  "balance",
+  "dihedral",
+  "slab",
+  "mantle",
+  "gaston"
+]
+
+puts "Creating base Styles..."
+styles.each do |style|
+  Style.create(name: style)
+end
+
+# seed DB with dummy data
+# users and their respective problems
+puts "Creating sample Users..."
+user_1 = User.new({username: "jackie", password: "12345"})
+user_2 = User.new({username: "sam", password: "54321"})
+user_3 = User.new({username: "natalie", password: "67890"})
+user_1.save!
+user_2.save!
+user_3.save!
+
