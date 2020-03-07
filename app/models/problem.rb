b33ee72c9b794ac8001b91ac18d33fa4158dd675
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-03-07 15:54:14 -0500

    Add ActiveRecord associations among all models

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,2 +1,7 @@
 class Problem < ApplicationRecord
+  belongs_to :wall
+  has_many :sends
+  has_many :users, through: :sends
+  has_many :problem_styles
+  has_many :styles, through: :problem_styles
 end
--- app/models/send.rb
+++ app/models/send.rb
@@ -1,2 +1,4 @@
 class Send < ApplicationRecord
+  belongs_to :user
+  belongs_to :problem
 end
