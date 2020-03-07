2767d8267c5834c9d1ad96899408e3eb958dc3c1
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-03-07 16:58:18 -0500

    Add constants to Problem model, delete useless routes - wall#create, styles#creae and problem_styles#create

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
     resources :sends
   end 
   resources :problems, only: [:index, :show, :new, :create]
-  resources :walls, only: :create
-  resources :styles, only: :create
-  resources :problem_styles, only: :create
+  # resources :walls, only: :create
+  # resources :styles, only: :create
+  # resources :problem_styles, only: :create
   resources :rewards, only: [:index, :show, :new, :create]
 end
